010923ac973bfd65ef3476fceb2bc8f06323baa1
Maciej Mroz <Mroz.M@eplan.pl>
2019-05-31 13:51:30 +0200

    end of section 3

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
+
+@Component({
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "A test recipe",
+      "This is a test",
+      "https://www.tasteofhome.com/wp-content/uploads/2018/01/Garlic-Lemon-Shrimp_EXPS_SDON16_45286__D06_09_5b-696x696.jpg"
+    ),
+    new Recipe(
+      "A test recipe",
+      "This is a test",
+      "https://www.tasteofhome.com/wp-content/uploads/2018/01/Garlic-Lemon-Shrimp_EXPS_SDON16_45286__D06_09_5b-696x696.jpg"
+    )
+  ];
+  constructor() {}
+
+  ngOnInit() {}
+}
