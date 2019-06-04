470c039b895966345d548ce4715a3d693a630c87
Maciej Mroz <Mroz.M@eplan.pl>
2019-06-04 15:12:19 +0200

    section 6, 86

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,9 @@ import { Component } from "@angular/core";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {}
+export class AppComponent {
+  pageSwitcher = "recipes";
+  onNavigate(event: string) {
+    this.pageSwitcher = event;
+  }
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
+  @Output() navigate = new EventEmitter<string>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  navigateRecipes() {
+    this.navigate.emit("recipes");
   }
 
+  navigateShoppingList() {
+    this.navigate.emit("shoppingList");
+  }
 }
